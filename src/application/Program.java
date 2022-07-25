f300ef6ce1591907aaf9f7b851f2a18b8da53f44
Rennan Bento <rennan.rodrigues09@gmail.com>
2022-07-25 19:08:12 -0300

    FindAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,15 +8,15 @@ import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
- 
+
 	public static void main(String[] args) {
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("=== TEST 1: seller findByTd ===== ");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("=== TEST 2: seller findByDepartment ===== ");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
@@ -24,6 +24,10 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("=== TEST 3: seller findAll ===== ");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);	
+		}
 	}
-
 }
